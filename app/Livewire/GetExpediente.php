18944f7424f6811b91c42bb95e4dc986f0fcad7b
Ryzen from R900 <echos999@gmail.com>
2024-06-14 23:55:26 -0300

    fix: troca do disparo do evento refresh pelos metodos mount e render. Assim força a reinicialização e atualização do componente.

--- app/Livewire/GetExpediente.php
+++ app/Livewire/GetExpediente.php
@@ -4,29 +4,32 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\Expediente;
 use Illuminate\Support\Facades\Auth;
-use Livewire\Attributes\On; 
+use Livewire\Attributes\On;
 
 
 class GetExpediente extends Component
 {
-    public $expedientes; 
+    public $expedientes;
    //descobrir forma de exibir na ordem dos dias da semana
-    public function mount(){ 
+    public function mount(){
         $this->expedientes = Expediente::where('id_voluntario', Auth::user()->id )->orderByRaw("FIELD(diaSemana, 'segunda', 'terca',
-        'quarta', 'quinta', 'sexta')")->get(); 
+        'quarta', 'quinta', 'sexta')")->get();
     }
     //Consigo acessar Auth:user() dentro do componente Livewire
     //porque ele é uma extensão do Laravel e compartilha o mesmo ciclo de vida
 
-    #[On('atualiza-expedientes')] 
+    #[On('atualiza-expedientes')]
     public function refreshComponent(){
         // dd('atualizou');
-        $this->dispatch('$refresh'); 
+        // $this->dispatch('refresh');
+        $this->mount();
+        $this->render();
     }
+
     public function render()
     {
-        // dd($this->expedientes); 
+        // dd($this->expedientes);
         return view('livewire.get-expediente');
     }
-   
+
 }
