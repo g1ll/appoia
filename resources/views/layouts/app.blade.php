7540a52d9f1e5ef1b208aaaf8156f64f540067c1
Ryzen from R900 <echos999@gmail.com>
2024-06-14 23:51:31 -0300

    fix: ajustes de importação e conflitos do livewire e alpinejs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html> <!--layout básico, onde são carregados os conteúdos internos--> 
+<!DOCTYPE html> <!--layout básico, onde são carregados os conteúdos internos-->
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @livewireStyles
-        @livewireScripts
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <title>{{ config('app.name', 'Laravel') }}</title>
@@ -23,8 +22,8 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <header class="bg-white shadow dark:bg-gray-800">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
@@ -35,5 +34,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @livewireScripts
     </body>
 </html>
